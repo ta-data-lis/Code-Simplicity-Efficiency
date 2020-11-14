60be8103a252366abaef5a85f8a8c5a49ace08dc
eloi <egf1995@gmail.com>
2020-11-14 16:57:43 +0100

    finished on time ;)

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -6,33 +6,19 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import random
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
-
-def BatchStringGenerator(n, a=8, b=12):
+def BatchStringGenerator():
+    min = input('Enter minimum string length: ')
+    max = input('Enter maximum string length: ')
+    n = int(input('How many random strings to generate? '))
     r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+    Characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(0, int(n)):
+        s = []
+        for y in range(0, random.choice(range(int(min), int(max)))):
+            s.append(random.choice(Characters))
+        r.append(s)
     return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
-
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator())
--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -1,7 +1,6 @@
 """
 You are presented with an integer number larger than 5. Your goal is to identify the longest side
 possible in a right triangle whose sides are not longer than the number you are given.
-
 For example, if you are given the number 15, there are 3 possibilities to compose right triangles:
 
 1. [3, 4, 5]
@@ -12,19 +11,6 @@ The following function shows one way to solve the problem but the code is not id
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
 
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+max = int(input("What is the maximal length of the triangle side? Enter a number bigger than 5: "))
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
-
-print("The longest side possible is " + str(my_function(int(X))))
+print("The longest side possible is ", list([x,y,z] for x in range(5, max) for y in range(4, max) for z in range(3, max) if (x*x==y*y+z*z))[-1])
