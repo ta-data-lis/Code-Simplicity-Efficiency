a2d4f4be5750578f21652c03e6849d87511ee308
Abdou-Man-24 <72160783+Abdou-Man-24@users.noreply.github.com>
2020-11-21 18:41:06 +0100

    solved_lab_code_simplicity

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -7,29 +7,22 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
+import sys
+
+elements=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+
+RandomStringGenerator = lambda x: ''.join([random.choice(elements) for ele in range(x)])
 
 def BatchStringGenerator(n, a=8, b=12):
-    r = []
     for i in range(n):
-        c = None
         if a < b:
-            import random
-            c = random.choice(range(a, b))
+             c = random.choice(range(a, b))
         elif a == b:
-            c = a
+             c = a
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+    return [RandomStringGenerator(c) for element in range(n)]
 
 a = input('Enter minimum string length: ')
 b = input('Enter maximum string length: ')
--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -13,17 +13,8 @@ Refactor the code based on what you have learned about code simplicity and effic
 """
 
 def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+    solutions = [[x,y,z] for x in range(5, X) for y in range(4, X) for z in range(3, X) if (x*x==y*y+z*z)]
+    return solutions[-1][0]
 
 X = input("What is the maximal length of the triangle side? Enter a number: ")
 
